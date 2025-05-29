e98bf00f8db6f91341b2ddff19e2448c5e22b967
Ivan Luengo <ivluengo@gmail.com>
2025-05-29 13:51:23 +0200

    change TODO title

--- src/main.js
+++ src/main.js
@@ -37,18 +37,18 @@ let allToDos = toDosLocalStorage || [];
 
 const $clearCompletedButton = document.querySelector('.clear-completed');
 const $toDoList = document.querySelector('.toDo-list');
-const $darkModeButton = document.querySelector('[aria-label="dark mode button"]');
+const $darkModeButton = document.querySelector(
+  '[aria-label="dark mode button"]'
+);
 const $filterCompleted = document.querySelector('.filter-completed');
 const $filterNotCompleted = document.querySelector('.filter-not-completed');
 const $filterAll = document.querySelector('.filter-all');
-
 
 // Imprimir los todos al cargar la página
 printToDos();
 
 // Rellenar el número de items que faltan por completar
 countItemsLeft();
-
 
 // Cazamos el formulario
 const $addToDoForm = document.querySelector('.add-todo-form');
@@ -61,38 +61,38 @@ $clearCompletedButton.addEventListener('click', clearCompletedToDos);
 
 // Escuchamos el click del botón de modo oscuro
 $darkModeButton.addEventListener('click', () => {
-  document.body.classList.toggle('dark');})
+  document.body.classList.toggle('dark');
+});
 
-function printToDos (arrayToDos = allToDos) {
+function printToDos(arrayToDos = allToDos) {
   // Borrar toda la lista de tareas
   $toDoList.innerHTML = '';
 
   // Recorro cada tarea de la lista de tareas
-  // for (const toDo of allToDos) {   
+  // for (const toDo of allToDos) {
   arrayToDos.forEach((toDo) => {
-    const article = createToDoHTML(toDo);  
+    const article = createToDoHTML(toDo);
     // Lo meto en el DOM dentro de la sección
-    $toDoList.append(article);  
-
+    $toDoList.append(article);
   });
 }
 
-function createToDoHTML (toDo) {
+function createToDoHTML(toDo) {
+  // Creo un nuevo "article" por cada uno
+  const article = document.createElement('article');
 
-    // Creo un nuevo "article" por cada uno
-    const article = document.createElement('article');
-  
-    // Le pongo las clases que necesita el article
-    article.className = 'bg-white py-2 px-4 first:rounded-t flex items-center gap-3  border-b border-b-gray-300 dark:bg-gray-700 dark:border-b-gray-00 dark:text-white';
-  
-    // Crear una variable para saber si el input estará checked o no
-    const isChecked = toDo.isCompleted ? 'checked' : '';
-  
-    // Crear una variable para saber si el check icon estará visible o no
-    const isCheckIconVisible = toDo.isCompleted ? '' : 'hidden';
-  
-    // Le pongo lo de dentro
-    article.innerHTML = `
+  // Le pongo las clases que necesita el article
+  article.className =
+    'bg-white py-2 px-4 first:rounded-t flex items-center gap-3  border-b border-b-gray-300 dark:bg-gray-700 dark:border-b-gray-00 dark:text-white';
+
+  // Crear una variable para saber si el input estará checked o no
+  const isChecked = toDo.isCompleted ? 'checked' : '';
+
+  // Crear una variable para saber si el check icon estará visible o no
+  const isCheckIconVisible = toDo.isCompleted ? '' : 'hidden';
+
+  // Le pongo lo de dentro
+  article.innerHTML = `
     <input aria-label="Complete to-do checkbox" id="${toDo.id}" class="complete-checkbox hidden peer" type="checkbox" ${isChecked}>
     <label data-complete="${toDo.id}" for="${toDo.id}" class="w-4 h-4 border border-gray-400 rounded-full flex items-center justify-center peer-checked:bg-check-gradient">
       <svg class="${isCheckIconVisible} check-icon" xmlns="http://www.w3.org/2000/svg" width="10" height="9"><path fill="none" stroke="#FFF" stroke-width="2" d="M1 4.304L3.696 7l6-6"/></svg>
@@ -105,51 +105,49 @@ function createToDoHTML (toDo) {
     </button>
     `;
 
-    //! OPCION 1 PARA ESCUCHAR LOS EVENTOS DE LOS BOTONES DE LOS TO-DOS
-    //* Justo aquí debajo, el article YA TIENE DENTRO el input y el Button que completan y borran el toDo.
-    // const completedInput = article.querySelector('.complete-checkbox');
-    // completedInput.addEventListener('change',  () => toggleCompleteToDo(toDo.id));        
+  //! OPCION 1 PARA ESCUCHAR LOS EVENTOS DE LOS BOTONES DE LOS TO-DOS
+  //* Justo aquí debajo, el article YA TIENE DENTRO el input y el Button que completan y borran el toDo.
+  // const completedInput = article.querySelector('.complete-checkbox');
+  // completedInput.addEventListener('change',  () => toggleCompleteToDo(toDo.id));
 
-    //* Ahora caza el boton de borrar
-    //* Le escuchas el click    
-    // const deleteButton = article.querySelector('[data-delete-button]');
-    // deleteButton.addEventListener('click', () => deleteToDo(toDo.id));
+  //* Ahora caza el boton de borrar
+  //* Le escuchas el click
+  // const deleteButton = article.querySelector('[data-delete-button]');
+  // deleteButton.addEventListener('click', () => deleteToDo(toDo.id));
 
-    //! ***************************************************/    
-    
-    return article;
+  //! ***************************************************/
+
+  return article;
 }
 
-function countItemsLeft () {
+function countItemsLeft() {
   const itemsLeftOutput = document.querySelector('.items-left-output');
-  
+
   let itemsLeftCount = 0;
   for (const toDo of allToDos) {
     if (toDo.isCompleted === false) {
       itemsLeftCount++;
     }
   }
-  
+
   itemsLeftOutput.innerText = itemsLeftCount;
 }
 
-function handleAddToDoFormSubmit (event) {
-  
+function handleAddToDoFormSubmit(event) {
   // Paramos el comportamiento por defecto del formulario
   event.preventDefault();
-  
+
   // Cogemos el valor del usuario
   const newTaskValue = $addToDoForm.addToDoInput.value;
-  
 
-  //! Oye, mejor frena si te lo dejan vacío 
-  if(isInputEmpty(newTaskValue)) {
+  //! Oye, mejor frena si te lo dejan vacío
+  if (isInputEmpty(newTaskValue)) {
     return;
   }
-   
+
   // Creamos un nuevo TODO que meteremos en el array
   createNewToDo(newTaskValue);
-  
+
   // Reimprimimos todos los todos
   printToDos();
 
@@ -166,50 +164,74 @@ function handleAddToDoFormSubmit (event) {
   setTodosInLocalStorage();
 }
 
-function createNewToDo (newTaskValue) {
-  const newToDo = {
-    id: nanoid(5),
-    task:  newTaskValue,
-    isCompleted: false
-  };
+// class ToDo {
+//   id = nanoid(5);
+//   isCompleted = false;
+
+//   constructor(task) {
+//     this.task = task;
+//   }
+
+//   toggleComplete() {
+//     this.isCompleted = !this.isCompleted;
+//   }
+// }
+
+function createNewToDo(newTaskValue) {
+  // const newToDo = {
+  //   id: nanoid(5),
+  //   task:  newTaskValue,
+  //   isCompleted: false
+  // };
+  const newToDo = new ToDo(newTaskValue);
 
   allToDos.push(newToDo);
 }
 
-function isInputEmpty (value) {
+function isInputEmpty(value) {
   if (value === '') {
-    $addToDoForm.addToDoInput.classList.replace('focus-visible:ring-2', 'ring-4')
-    $addToDoForm.addToDoInput.classList.replace('focus-visible:ring-pink-500', 'ring-red-500')
+    $addToDoForm.addToDoInput.classList.replace(
+      'focus-visible:ring-2',
+      'ring-4'
+    );
+    $addToDoForm.addToDoInput.classList.replace(
+      'focus-visible:ring-pink-500',
+      'ring-red-500'
+    );
     return true;
   } else {
-    $addToDoForm.addToDoInput.classList.replace('ring-4', 'focus-visible:ring-2')
-    $addToDoForm.addToDoInput.classList.replace('ring-red-500', 'focus-visible:ring-pink-500')
+    $addToDoForm.addToDoInput.classList.replace(
+      'ring-4',
+      'focus-visible:ring-2'
+    );
+    $addToDoForm.addToDoInput.classList.replace(
+      'ring-red-500',
+      'focus-visible:ring-pink-500'
+    );
     return false;
   }
 }
 
-function toggleCompleteToDo (idToComplete) {  
+function toggleCompleteToDo(idToComplete) {
   // for (const i in allToDos) {
   //   const toDo = allToDos[i];
-  
+
   //   if (toDo.id === idToComplete) {
   //     toDo.isCompleted = !toDo.isCompleted;
   //     break;
-  //   }  
-  // } 
-
+  //   }
+  // }
 
   const toDoToToggle = allToDos.find((toDo) => toDo.id === idToComplete);
   toDoToToggle.isCompleted = !toDoToToggle.isCompleted;
-  
+
   printToDos();
   countItemsLeft();
 
   setTodosInLocalStorage();
 }
 
-function deleteToDo (idToDelete) {
-
+function deleteToDo(idToDelete) {
   //* Opción 1: Bucle for y splice
   // for (const i in allToDos) {
   //   const toDo = allToDos[i];
@@ -225,7 +247,9 @@ function deleteToDo (idToDelete) {
   // allToDos.splice(indexToDoToDelete, 1);
 
   //* Opción 3: filter 👉 Crea un nuevo array en memoria, es decir el original deja de existir
-  allToDos = allToDos.filter((toDo) => { return toDo.id !== idToDelete });
+  allToDos = allToDos.filter((toDo) => {
+    return toDo.id !== idToDelete;
+  });
 
   printToDos();
   countItemsLeft();
@@ -233,12 +257,11 @@ function deleteToDo (idToDelete) {
   setTodosInLocalStorage();
 }
 
-function setTodosInLocalStorage () {
+function setTodosInLocalStorage() {
   localStorage.setItem('toDos', JSON.stringify(allToDos));
 }
 
-
-function clearCompletedToDos () {  
+function clearCompletedToDos() {
   // for (let i = 0; i < allToDos.length; i++) {
   //   const toDo = allToDos[i];
 
@@ -248,52 +271,44 @@ function clearCompletedToDos () {
   //   }
   // }
 
-
   //* Opción 2: filter
   allToDos = allToDos.filter((toDo) => toDo.isCompleted === false);
 
   printToDos();
   countItemsLeft();
   setTodosInLocalStorage();
-
 }
-
-
 
 //! OPCION 2 PARA ESCUCHAR LOS EVENTOS DE LOS BOTONES DE LOS TO-DOS
 //? Event Delegation
-
 
 $toDoList.addEventListener('click', (event) => {
   const deleteButton = event.target.closest('button[data-delete]');
   const completeLabel = event.target.closest('label[data-complete]');
 
   if (deleteButton !== null) {
-    const toDoId = deleteButton.dataset.delete;    
+    const toDoId = deleteButton.dataset.delete;
     deleteToDo(toDoId);
-
   } else if (completeLabel !== null) {
     const toDoId = completeLabel.dataset.complete;
     toggleCompleteToDo(toDoId);
   }
-  
 });
-
-
 
 //* Filtros
 
 $filterCompleted.addEventListener('click', () => {
-  const filteredCompleted = allToDos.filter((toDo) => {return toDo.isCompleted === true })
+  const filteredCompleted = allToDos.filter((toDo) => {
+    return toDo.isCompleted === true;
+  });
   printToDos(filteredCompleted);
 });
 
 $filterAll.addEventListener('click', () => printToDos());
 
 $filterNotCompleted.addEventListener('click', () => {
-  const filteredNotCompleted = allToDos.filter((toDo) => {return toDo.isCompleted === false })
+  const filteredNotCompleted = allToDos.filter((toDo) => {
+    return toDo.isCompleted === false;
+  });
   printToDos(filteredNotCompleted);
 });
-
-
-
